cc59565b8034bc354e214257345182af0176549e
Ksenia Tuzova <102681427+Ksenia-Tuzova2@users.noreply.github.com>
2023-04-21 23:27:24 +0300

    new dir and files

---
+++ pages/authorization.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Authorization() {
+  return (
+    <div>
+
+
+    </div>
+
+  );
+}
+
---
+++ pages/game.tsx
@@ -0,0 +1,12 @@
+import IconBar from "../shared/shared/iconBar";
+
+export default function Game() {
+  return (
+    <div>
+
+      <IconBar />
+    </div>
+
+  );
+}
+
